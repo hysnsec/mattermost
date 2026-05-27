d67bf1acfb5a069e5e81a70bf9c584c4afc144a0
ajay.kumar <ajay@practical-devsecops.com>
2026-05-27 11:59:42 +0530

    Fix: remove Team Edition badge from signup/login header

--- webapp/channels/src/components/header_footer_route/header.tsx
+++ webapp/channels/src/components/header_footer_route/header.tsx
@@ -26,12 +26,7 @@ const Header = ({alternateLink, backButtonURL, onBackButtonClick}: HeaderProps) 
 
     const ariaLabel = SiteName || 'Mattermost';
 
-    let freeBanner = null;
-    if (license.IsLicensed === 'false') {
-        freeBanner = <><Logo/><span className='freeBadge'>{'TEAM EDITION'}</span></>;
-    } else if (license.SkuShortName === LicenseSkus.Entry) {
-        freeBanner = <><Logo/><span className='freeBadge'>{'ENTRY EDITION'}</span></>;
-    }
+    const freeBanner = null;
 
     let title: React.ReactNode = SiteName;
     if (title === 'Mattermost') {
